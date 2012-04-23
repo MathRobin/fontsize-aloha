8b06cf7934f4f16272c91c43c1906d205ddb78a4
Mathieu ROBIN <mrobin@companeo.com>
2012-04-23 16:02:22 +0200

    ajout des fichiers

---
+++ nls/fr/i18n.js
@@ -0,0 +1,5 @@
+define({
+	"floatingmenu.tab.fontformat" : "Police",
+	"button.font.increase.tooltip" : "Augmenter la taille de la police",
+	"button.font.decrease.tooltip" : "Réduire la taille de la police"
+});
---
+++ nls/i18n.js
@@ -0,0 +1,13 @@
+define({
+	root : {
+		"floatingmenu.tab.fontformat" : "Fonts",
+		"button.font.increase.tooltip" : "Increase font size",
+		"button.font.decrease.tooltip" : "Decrease font size"
+	},
+	"de" : false,
+	"eo" : false,
+	"fi" : false,
+	"fr" : true,
+	"it" : false,
+	"ru" : false
+});
